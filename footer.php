f2826104a21c85228d559efae00788b83df67f4a
David Adeyemi Ogunye <oladimejid@gmail.com>
2023-07-27 09:30:12 +0100

    new commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+
+<footer>
+	<section class="container">
+	<div class="row">
+        
+        <div class="col-md-4" id="contact">
+        	<h3>Contact info</h3>
+                <ul>
+                        <li><i class="icon-home"></i> Block 223 low cost H/Estate Isolo-lagos</li>
+                        <li><i class="icon-phone"></i> Telephone: (+234)7048778559</li>
+                        <li><i class="icon-envelope"></i> Email: <a href="info@yonevas.institute">info@yonevas.institute</a></li>
+                    </ul>    
+        </div>
+        
+        <div class="col-md-4">
+            <ul class="social-bookmarks clearfix">
+            	<li class="youtube"><a href="https://www.youtube.com/channel/UC7--ERLy6wd7UjvDqepxReA">Youtube</a></li>
+                    <li class="facebook"><a href="https://www.facebook.com/Yonevascollege">facebook</a></li>
+            </ul>
+        </div>
+        
+    </div><!-- end row -->
+  	</section>
+    
+    <section id="footer_2">
+    <div class="container">
+    <div class="row">
+    	<div class="col-md-6">
+                <ul class="social-bookmarks clearfix">
+                	<li>Copyright© Yonevas College </li>
+                </ul>              
+        </div>
+        </div>
+		</div>
+    </section>
+
+</footer> 
+ 
