81d63de12fbf516c0dca022e963d62e0ed272e44
David Adeyemi Ogunye <oladimejid@gmail.com>
2023-08-06 20:49:15 +0100

    initial commit

---
+++ superadmin/action.php
@@ -0,0 +1,19 @@
+<?php
+include('../admin/classUser.php');
+$user = new User();
+if(!empty($_POST['action']) && $_POST['action'] == 'listUser') {
+	$user->getUserList();
+}
+if(!empty($_POST['action']) && $_POST['action'] == 'userDelete') {
+	$user->deleteUser();
+}
+if(!empty($_POST['action']) && $_POST['action'] == 'getUser') {
+	$user->getUser();
+}
+if(!empty($_POST['action']) && $_POST['action'] == 'addUser') {
+	$user->addUser();
+}
+if(!empty($_POST['action']) && $_POST['action'] == 'updateUser') {
+	$user->updateUser();
+}
+?>
