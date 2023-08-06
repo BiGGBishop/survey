94cc43cd8fe8c727ce9d81cff7a2dba82419bb7b
David Adeyemi Ogunye <oladimejid@gmail.com>
2023-08-06 20:44:48 +0100

    initial commit

---
+++ admin/include/conn.php
@@ -0,0 +1,9 @@
+<?php
+ 
+//MySQLi Procedural
+$conn = mysqli_connect("localhost","yonevasi_survey","ade-yemi123","yonevasi_survey");
+if (!$conn) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+ 
+?>
